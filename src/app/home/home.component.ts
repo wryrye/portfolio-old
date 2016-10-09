a224a67245a71e41b45661a5589ce8c09deccd1a
Ryan Coughlin <rlc4sv@virginia.edu>
2016-10-08 22:51:14 -0400

    -added speech bubble and question-wrapper -added scroll for question-wrapper overflow

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -44,8 +44,18 @@ export class Home  {
     @ViewChild(ColorSelectComponent)
     private colorSelectComponent: ColorSelectComponent;
 
+    ngOnInit(){
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            console.log('hello mobile user!');
+            $(".question").css("font-size", "2em");
+            $(".button").css("font-size", "1em");
+            $("#replay").css("font-size", "4em");
+        }
+    }
 
     ngAfterViewInit(){
+
+        //use CSS animation endings for signalling/transitioning
 
         $("#loaded")
             .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
@@ -55,8 +65,6 @@ export class Home  {
                     $("#knight").css("animation-name", "moveOnScreen"); //and animate him!
                 },this));
 
-        //use CSS animation endings for signalling/transitioning
-
         $("#knight")
             .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
                 jQuery.proxy(function(e){
@@ -65,42 +73,46 @@ export class Home  {
                     }
 
                     if ($("#knight").css("animation-name") == "moveOnScreen") { //when knight enters window 1st time...
-                        $("#name").css("animation-name","fadein")
-                        $("#name").css("opacity","1")
-                        $("#speech").css("animation-name","fadein")
-                        $("#speech").css("opacity","1")
+                        $("#name").css("animation-name","fadein");
+                        $("#name").css("opacity","1");
+                        $("#name").css("visibility", "visible");
+
+                        $("#speech").css("animation-name","fadein");
+                        $("#speech").css("opacity","1");
+                        $("#speech").css("visibility", "visible");
+
+                        $("#question-wrapper").css("visibility", "visible");
 
                     }
 
-                    if ($("#knight").css("animation-name") == "crossBridge") {//when night retreives resume...
-                        // if(this.questSelectComponent.quest == "holygrail") {
-                        //     $("#holygrail").css("visibility", "visible");
-                        //     $("#holygrail").css("animation-name", "grow");
-                        // }
-                        // else {
-                            $("#theCanvas").css("visibility", "visible");  //make canvas visible
-                            $("#theCanvas").css("animation-name", "grow");// and expand it
-                            $("#theCanvas").css("animation-duration", "2s");
+                    if ($("#knight").css("animation-name") == "crossBridge") {//when night retreives item...
 
-                            $("#name").css("visibility", "hidden");  //and hide the question elements
-                            $("#quest").css("visibility", "hidden");
-                            $("#color").css("visibility", "hidden");
-                            $("#speech").css("visibility", "hidden");
-                        // }
+                        if(this.questSelectComponent.quest == "holygrail") {
+                            $("#theCanvas").css("visibility", "visible");
+                            $("#theCanvas").css("animation-name", "fadein");
+                        }
+                        else {
+                            $("#theCanvas").css("visibility", "visible");  //make canvas visible
+                            $("#theCanvas").css("animation-name", "grow");// and expand it ***
+                        }
+
+                        $("#theCanvas").css("animation-duration", "2s");
                     }
                     //note: jQuery.proxy allows reference to 'this'
                 },this));
 
         $("#theCanvas")
             .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
-                function(e){ //when resume expands completely
+                jQuery.proxy(function(e){ //when resume expands completely
                     $("#close").css("visibility","visible"); //show close button
                     $("#knight").css("transform","none"); //reset knight direction
                     $("#knight").css("left","21%"); //and position
                     $("#poof").css("animation","toggle"); //toggle-reset poof cloud
                     $("#gatekeeper").css("visibility","visible"); //show gatekeeper
-                    $("#boss").css("overflow", "scroll"); //enable scrolling
-                });
+                     if(this.questSelectComponent.quest != "holygrail") {
+                        $("#wrapper").css("overflow-y", "scroll");
+                     } //enable scrolling
+                },this));
 
 
         $("#color")
@@ -110,15 +122,24 @@ export class Home  {
                         $("#poof").css("animation", "explode"); //explode the poof
                         $("#poof").css("animation-duration", "1.5s");
 
-                        // $("#boss").css("animation", "blur");
-                        // $("#boss").css("animation-duration", ".5s");
-
                         $("#gatekeeper").css("visibility", "hidden");//gatekeeper apparates!
 
                         $("#knight").css("animation", "crossBridge"); //knight crosses the bridge
                         $("#knight").css("animation-duration", "6s");
                         $("#knight").css("transform", "scaleX(-1)"); //and turns around
                         $("#knight").css("left", "35%");
+                    }
+                },this));
+
+        $("#speech")
+            .on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+                jQuery.proxy(function(e){
+                    if($("#speech").css("animation-name") == "fadeout"  ) { //when color question fades out (wait for 2nd animation with opacity check)
+                        $("#question-wrapper").css("visibility", "hidden");
+                        $("#name").css("visibility", "hidden");  //and hide the question elements
+                        $("#quest").css("visibility", "hidden");
+                        $("#color").css("visibility", "hidden");
+                        $("#speech").css("visibility", "hidden");
                     }
                 },this));
 
@@ -132,6 +153,7 @@ export class Home  {
         $("#name").css("z-index","0");
         $("#quest").css("animation-name","fadein")
         $("#quest").css("opacity","1")
+        $("#quest").css("visibility", "visible");
         this.name = this.nameSelectComponent.name;
     }
     questToColor(){ //fade out quest question and fadein color question
@@ -140,12 +162,13 @@ export class Home  {
         $("#quest").css("z-index","0");
         $("#color").css("animation-name","fadein")
         $("#color").css("opacity","1")
+        $("#color").css("visibility", "visible");
         this.quest = this.questSelectComponent.quest;
 
-        //convenient spot to toggle animations to be set in the future
+        //convenient spot to toggle animations to be set again in the future
         $("#knight").css("animation","toggle");
         $("#theCanvas").css("animation-name", "toggle");
-        $("#boss").css("animation-name", "toggle");
+        $("#wrapper").css("animation-name", "toggle");
         $("#replay").css("animation-name", "toggle");
     }
 
@@ -195,6 +218,7 @@ export class Home  {
         }).catch(function (reason) {
             console.error('Error: ' + reason);
         });
+        $("#theCanvas").css("height","auto");
     }
 
     showEssay(){
@@ -235,6 +259,7 @@ export class Home  {
         }).catch(function (reason) {
             console.error('Error: ' + reason);
         });
+        $("#theCanvas").css("height","auto");
     }
 
     showGrail() {
@@ -242,14 +267,17 @@ export class Home  {
         var canvas = document.getElementById('theCanvas');
         var ctx=(<HTMLCanvasElement> canvas).getContext("2d");
         var img = document.getElementById("holygrail");
-        ctx.drawImage(img, 10, 2)
+        (<HTMLInputElement>canvas).width = "660";
+        (<HTMLInputElement>canvas).height = "364";
+        ctx.drawImage(img, 0, 0,660,364);
+        $("#theCanvas").css("height","100%");
     }
 
     //closes Resume
     closeDocument(){
         $("#theCanvas").css("visibility","hidden"); //hides resume canvas
         $("#close").css("visibility","hidden"); //hides close button
-        $("#boss").css("overflow", "hidden"); //disables scroll
+        $("#wrapper").css("overflow-y", "hidden"); //disables scroll
 
         $("#replay").css("visibility","visible"); //show replay button
         $("#replay").css("animation-name","fadein");
@@ -258,29 +286,26 @@ export class Home  {
     //launch knight into the abyss when he lies about his own favorite color?!
     launch(){
         $("#knight").css("animation", "launch");
-        $("#knight").css("animation-duration", "2s");
+        $("#knight").css("animation-duration", "1s");
     }
 
     //reset game to initial values
     replay(){
         console.log("replay!!!")
-        // $("#name").css("animation-name","fadein")
-        // $("#name").css("opacity","1")
-        // $("#speech").css("animation-name","fadein")
-        // $("#speech").css("opacity","1")
-
-        $("#name").css("visibility", "visible");  //and hide the question elements
-        $("#quest").css("visibility", "visible");
-        $("#color").css("visibility", "visible");
-        $("#speech").css("visibility", "visible");
         $("#name").css("z-index","3");
         $("#quest").css("z-index","2");
         $("#color").css("z-index","1");
         $("#replay").css("visibility","hidden");
 
-        $("#boss").css("animation", "blur");
-        $("#boss").css("animation-duration", ".5s");
+        $("#wrapper").css("animation", "blur");
+        $("#wrapper").css("animation-duration", "1s");
         $("#knight").css("animation-name", "moveOnScreen");
         $("#knight").css("animation-duration", "3s");
+    }
+
+    testGrail(){
+        $("#knight").css("animation", "crossBridge"); //knight crosses the bridge
+        $("#knight").css("animation-duration", "1s");
+        this.showGrail();
     }
 }
